5a489375b724e94ccd70205b6bf05188eb52d542
pudy248 <72939948+pudy248@users.noreply.github.com>
2023-06-09 16:47:23 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
-local compatibleModList = {
-    "copis_things",
-    "nightmare",
-}
-
 --hax for copying data overrides
 local dataFiles = {
     copis_things = {
@@ -17,24 +12,30 @@ local dataFiles = {
     }
 }
 
-local installedModList = {}
-for _,v in ipairs(compatibleModList) do
-    if(ModTextFileGetContent("mods/"..v.."/init.lua") ~= nil) then installedModList[#installedModList+1] = v end
-end
-for k,v in ipairs(compatibleModList) do
-    print("Compatible mod #"..k..": "..v)
+local copisThingsInstalled = ModTextFileGetContent("mods/copis_things/init.lua") ~= nil
+local copisThingsEnabled = false
+
+local enabledModList = ModGetActiveModIDs()
+for _,v in ipairs(enabledModList) do
+    if v == "copis_things" then copisThingsEnabled = true end
 end
 
---make more random later
-local selectedMod = "copis_things"
+if copisThingsInstalled and not copisThingsEnabled then
+    --make more random later
+    local selectedMod = "copis_things"
 
-if(dataFiles[selectedMod] ~= nil) then
-    for k,v in ipairs(dataFiles[selectedMod]) do
-        ModTextFileSetContent("data/"..v, ModTextFileGetContent("mods/"..selectedMod.."/data/"..v))
+    print(ModTextFileGetContent("data/"))
+
+    if(dataFiles[selectedMod] ~= nil) then
+        for k,v in ipairs(dataFiles[selectedMod]) do
+            ModTextFileSetContent("data/"..v, ModTextFileGetContent("mods/"..selectedMod.."/data/"..v))
+        end
     end
-end
 
-ModLuaFileAppend("mods/ModMimic/init.lua", "mods/"..selectedMod.."/init.lua")
-if(ModTextFileGetContent("mods/"..selectedMod.."/settings.lua") ~= nil) then
-    ModLuaFileAppend("mods/ModMimic/settings.lua", "mods/"..selectedMod.."/settings.lua")
+    ModLuaFileAppend("mods/ModMimic/init.lua", "mods/"..selectedMod.."/init.lua")
+    if(ModTextFileGetContent("mods/"..selectedMod.."/settings.lua") ~= nil) then
+        ModLuaFileAppend("mods/ModMimic/settings.lua", "mods/"..selectedMod.."/settings.lua")
+    end
+else
+    --translation stuff
 end
